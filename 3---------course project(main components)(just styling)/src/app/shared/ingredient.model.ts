db48f66f48fc773387a0430323f80aa92957ff2d
Abdallah Wahbah <40547258+abdallahWahbah@users.noreply.github.com>
2024-04-30 08:10:33 +0300

    course project(main components)(just styling)

---
+++ 3---------course project(main components)(just styling)/src/app/shared/ingredient.model.ts
@@ -0,0 +1,7 @@
+export class Ingredient
+{
+    // look at the recipe model >>>> this is a shortcut for the same assigning
+    constructor(public name: string, public amount: number)
+    {
+    }
+}
